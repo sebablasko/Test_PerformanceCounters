85ff49e4cc2e3f27f80fe4f20a693898e3c7056b
Sebastian <sebablasko@gmail.com>
2015-03-17 14:34:38 -0300

    actualiza run para guardar con record de perf

--- run_stats.sh
+++ run_stats.sh
@@ -2,10 +2,10 @@
 
 make all
 
-limite_threads=1
+limite_threads=4
 clients=4
 maxPackages=1000000
-repetitions=1
+repetitions=3
 
 #Contadores a revisar:
 #cycles :
@@ -43,7 +43,7 @@ for ((serverThreads=1 ; $serverThreads<=$limite_threads ; serverThreads=2*server
 	for ((i=1 ; $i<=$repetitions ; i++))		
 	{
 		echo $i" repetition"
-		perf stat -e cycles,cpu-cycles,ref-cycles,r10000013c,cache-references,cache-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-icache-loads,L1-icache-load-misses,cpu-migrations,r530263,r530163,r53012e,r53022e,r530324,r53aa24,r530126,r530426,r534026,r531026,r530428,r530128 ./server $maxPackages $serverThreads &
+		perf record -e cycles,cpu-cycles,ref-cycles,r10000013c,cache-references,cache-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-icache-loads,L1-icache-load-misses,cpu-migrations,r530263,r530163,r53012e,r53022e,r530324,r53aa24,r530126,r530426,r534026,r531026,r530428,r530128 ./server $maxPackages $serverThreads &
 
 		pid=$!
 		sleep 1
@@ -53,6 +53,9 @@ for ((serverThreads=1 ; $serverThreads<=$limite_threads ; serverThreads=2*server
 			./client $maxPackages 127.0.0.1 > /dev/null &
 		}
 		wait $pid
+
+		newFile=perf"{_}"$serverThreads"_"$i".data"
+		mv perf.data newFile
 	}
 
 }
