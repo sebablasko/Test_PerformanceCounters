041cb28665d73d34b128150f66f49c2018a641cf
Sebastian <sebablasko@gmail.com>
2015-03-19 11:46:35 -0300

    probando postprocesamiento

--- portProcessing.py
+++ portProcessing.py
@@ -1,19 +1,26 @@
 import sys
-filename = sys.argv[1]
-archivo = open(filename, 'r')
-
-threads = filename.split("_")[1]
-repetition = filename.split("_")[2]
-
+import os
+import glob
 eventos = {}
 
-for line in archivo:
-	if "%" in line:
-		registro = line.split(" ")
-		registro = filter(lambda x: x!="", registro)
-		registro = registro[0:2]
 
-		if registro[1] not in eventos:
-			eventos[registro[1]] = {threads : [registro[0]]}
+files = glob.glob(os.getcwd()+"/*.data")
+
+for filename in files:
+
+	#filename = sys.argv[1]
+	archivo = open(filename, 'r')
+
+	threads = filename.split("_")[1]
+	repetition = filename.split("_")[2]
+
+	for line in archivo:
+		if "%" in line:
+			registro = line.split(" ")
+			registro = filter(lambda x: x!="", registro)
+			registro = registro[0:2]
+
+			if registro[1] not in eventos:
+				eventos[registro[1]] = {threads : [registro[0]]}
 
 print eventos
