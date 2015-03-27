935680b633876b083576a8bebc12b60ceffc17fb
Sebastian <sebablasko@gmail.com>
2015-03-27 10:51:51 -0300

    agrega eventos del ppt

--- run_stats.sh
+++ run_stats.sh
@@ -2,7 +2,7 @@
 
 make all
 
-limite_threads=1
+limite_threads=2
 clients=4
 maxPackages=1000000
 repetitions=3
@@ -134,7 +134,7 @@ for ((serverThreads=1 ; $serverThreads<=$limite_threads ; serverThreads=2*server
 	{
 		#echo $i" repetition"
 		newFile=perfStat"_"$serverThreads"_"$i".data"
-		perf stat -e r500104, r500204, r500404, r500105, r500205, r500405, r500420, r500820, r500108, r500208, r500109, r500209, r50010a, r50020a, r50040a, r50080a, r500f0a, r50010b, r50020b, r50040b, r50080b, r50100b, r501f0b, r530426, r530126, r530326, r530526, r530451, r530251, r530851 -o $newFile ./server $maxPackages $serverThreads &
+		perf stat -e r500104,r500204,r500404,r500105,r500205,r500405,r500420,r500820,r500108,r500208,r500109,r500209,r50010a,r50020a,r50040a,r50080a,r500f0a,r50010b,r50020b,r50040b,r50080b,r50100b,r501f0b,r530426,r530126,r530326,r530526,r530451,r530251,r530851 -o $newFile ./server $maxPackages $serverThreads &
 
 		pid=$!
 		sleep 1
