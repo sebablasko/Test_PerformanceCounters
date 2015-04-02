8db2145b6e6ba3d7346106696aac20873ceee282
sebastian <sebastian@sebastian-VirtualBox.(none)>
2015-04-01 23:58:58 -0300

    actualiza run

--- run_stats.sh
+++ run_stats.sh
@@ -7,7 +7,7 @@ clients=4
 maxPackages=1000000
 repetitions=3
 
-counters=r500104, r500204, r500404, r500105, r500205, r500405, r500420, r500820, r530451, r530251, r530851, r500108, r500208, r500109, r500209, r50010a, r50020a, r50040a, r50080a, r500f0a, r50010b, r50020b, r50040b, r50080b, r50100b, r501f0b, r530426, r530126, r530326, r530526
+#counters=r500104, r500204, r500404, r500105, r500205, r500405, r500420, r500820, r530451, r530251, r530851, r500108, r500208, r500109, r500209, r50010a, r50020a, r50040a, r50080a, r500f0a, r50010b, r50020b, r50040b, r50080b, r50100b, r501f0b, r530426, r530126, r530326, r530526
 
 echo ""
 
@@ -38,8 +38,6 @@ echo ""
 python postProcessing.py
 mv SummaryResults.csv SummaryResults_UDPMultiThread.csv
 mv FullResults.csv FullResults_UDPMultiThread.csv
-
-rm *.data
 
 for ((serverThreads=1 ; $serverThreads<=$limite_threads ; serverThreads=2*serverThreads))
 {
