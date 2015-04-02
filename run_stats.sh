32553c796d4d712bcb90d26bd85806a3a56d754a
sebastian <sebastian@sebastian-VirtualBox.(none)>
2015-04-01 23:17:45 -0300

    actualiza run

--- run_stats.sh
+++ run_stats.sh
@@ -19,7 +19,7 @@ for ((serverThreads=1 ; $serverThreads<=$limite_threads ; serverThreads=2*server
 	{
 		#echo $i" repetition"
 		newFile=perfStat"_"$serverThreads"_"$i".data"
-		perf stat -e r500204, r500404, r500105, r500205, r500405, r500420, r500820, r530451, r530251, r530851, r500108, r500208, r500109, r500209, r50010a, r50020a, r50040a, r50080a, r500f0a, r50010b, r50020b, r50040b, r50080b, r50100b, r501f0b, r530426, r530126, r530326, r530526 -o $newFile ./server $maxPackages $serverThreads &
+		perf stat -e r500404, r500105, r500205, r500405, r500420, r500820, r530451, r530251, r530851, r500108, r500208, r500109, r500209, r50010a, r50020a, r50040a, r50080a, r500f0a, r50010b, r50020b, r50040b, r50080b, r50100b, r501f0b, r530426, r530126, r530326, r530526 -o $newFile ./server $maxPackages $serverThreads &
 
 		pid=$!
 		sleep 1
