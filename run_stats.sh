08c218f6f1ba095a9f6bf6e93e905dc63b6d40fc
Sebastian <sebablasko@gmail.com>
2015-04-07 16:53:17 -0300

    updated run

--- run_stats.sh
+++ run_stats.sh
@@ -17,9 +17,9 @@ for ((serverThreads=1 ; $serverThreads<=$limite_threads ; serverThreads=2*server
 
 	for ((i=1 ; $i<=$repetitions ; i++))		
 	{
-		#echo $i" repetition"
+		echo $i" repetition"
 		newFile=perfStat"_"$serverThreads"_"$i".data"
-		perf stat -e r500104,r500204,r500404,r500105,r500205,r500405,r500420,r500820,r530451,r530251,r530851,r500108,r500208,r500109,r500209,r50010a,r50020a,r50040a,r50080a,r500f0a,r50010b,r50020b,r50040b,r50080b,r50100b,r501f0b,r530426,r530126,r530326,r530526 -o $newFile -- ./server $maxPackages $serverThreads &
+		perf stat -e r500104,r500204,r500404,r500105,r500205,r500405,r500420,r500820,r530451,r530251,r530851,r500108,r500208,r500109,r500209,r50010a,r50020a,r50040a,r50080a,r500f0a,r50010b,r50020b,r50040b,r50080b,r50100b,r501f0b,r530426,r530126,r530326,r530526 -o $newFile -- ./server $maxPackages $serverThreads >> /dev/null &
 
 		pid=$!
 		sleep 1
@@ -47,8 +47,8 @@ for ((serverThreads=1 ; $serverThreads<=$limite_threads ; serverThreads=2*server
 
 	for ((i=1 ; $i<=$repetitions ; i++))		
 	{
-		#echo $i" repetition"
-		perf stat -e r500104,r500204,r500404,r500105,r500205,r500405,r500420,r500820,r530451,r530251,r530851,r500108,r500208,r500109,r500209,r50010a,r50020a,r50040a,r50080a,r500f0a,r50010b,r50020b,r50040b,r50080b,r50100b,r501f0b,r530426,r530126,r530326,r530526 -o $newFile -- ./dev_null $maxPackages $serverThreads &
+		echo $i" repetition"
+		perf stat -e r500104,r500204,r500404,r500105,r500205,r500405,r500420,r500820,r530451,r530251,r530851,r500108,r500208,r500109,r500209,r50010a,r50020a,r50040a,r50080a,r500f0a,r50010b,r50020b,r50040b,r50080b,r50100b,r501f0b,r530426,r530126,r530326,r530526 -o $newFile -- ./dev_null $maxPackages $serverThreads >> /dev/null &
 
 		pid=$!
 		wait $pid
