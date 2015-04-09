07824e3880b918856152ed4ae1c9b4ce8ca59ef1
sebastian <sebastian@sebastian-VirtualBox.(none)>
2015-04-08 23:13:53 -0300

    updated run

--- run_stats.sh
+++ run_stats.sh
@@ -2,62 +2,62 @@
 
 make all
 
-limite_threads=2
+limite_threads=16
 clients=4
 maxPackages=1000000
-repetitions=2
+repetitions=4
 
 #counters=r500104, r500204, r500404, r500105, r500205, r500405, r500420, r500820, r530451, r530251, r530851, r500108, r500208, r500109, r500209, r50010a, r50020a, r50040a, r50080a, r500f0a, r50010b, r50020b, r50040b, r50080b, r50100b, r501f0b, r530426, r530126, r530326, r530526
 
-# echo ""
-# echo "Testing UDP MultiThread Transmission"
-# for ((serverThreads=1 ; $serverThreads<=$limite_threads ; serverThreads=2*serverThreads))
-# {
-# 	echo $serverThreads" Threads"
+echo ""
+echo "Testing UDP MultiThread Transmission"
+for ((serverThreads=1 ; $serverThreads<=$limite_threads ; serverThreads=2*serverThreads))
+{
+	echo $serverThreads" Threads"
 
-# 	for ((i=1 ; $i<=$repetitions ; i++))		
-# 	{
-# 		echo $i" repetition"
-# 		newFile="perfStat_UDPMultiThread_"$serverThreads"_"$i".data"
-# 		perf stat -e r500104,r500204,r500404,r500105,r500205,r500405,r500420,r500820,r530451,r530251,r530851,r500108,r500208,r500109,r500209,r50010a,r50020a,r50040a,r50080a,r500f0a,r50010b,r50020b,r50040b,r50080b,r50100b,r501f0b,r530426,r530126,r530326,r530526 -o $newFile -- ./server $maxPackages $serverThreads 1820 >> /dev/null &
-# 		pid=$!
-# 		sleep 1
+	for ((i=1 ; $i<=$repetitions ; i++))		
+	{
+		echo $i" repetition"
+		newFile="perfStat_UDPMultiThread_"$serverThreads"_"$i".data"
+		perf stat -e r500104,r500204,r500404,r500105,r500205,r500405,r500420,r500820,r530451,r530251,r530851,r500108,r500208,r500109,r500209,r50010a,r50020a,r50040a,r50080a,r500f0a,r50010b,r50020b,r50040b,r50080b,r50100b,r501f0b,r530426,r530126,r530326,r530526 -o $newFile -- ./server $maxPackages $serverThreads 1820 >> /dev/null &
+		pid=$!
+		sleep 1
 
-# 		for ((j=1 ; $j<=$clients ; j++))
-# 		{
-# 			./client $(($maxPackages*10)) 127.0.0.1 1820 > /dev/null &
-# 		}
-# 		wait $pid
-# 	}
+		for ((j=1 ; $j<=$clients ; j++))
+		{
+			./client $(($maxPackages*10)) 127.0.0.1 1820 > /dev/null &
+		}
+		wait $pid
+	}
 
-# }
+}
 
-# python postProcessing.py
-# mv SummaryResults.csv SummaryResults_UDPMultiThread.csv
-# mv FullResults.csv FullResults_UDPMultiThread.csv
-# rm *.data
+python postProcessing.py
+mv SummaryResults.csv SummaryResults_UDPMultiThread.csv
+mv FullResults.csv FullResults_UDPMultiThread.csv
+rm *.data
 
-# echo ""
-# echo "Testing dev_null Transmission"
-# for ((serverThreads=1 ; $serverThreads<=$limite_threads ; serverThreads=2*serverThreads))
-# {
-# 	echo $serverThreads" Threads"
+echo ""
+echo "Testing dev_null Transmission"
+for ((serverThreads=1 ; $serverThreads<=$limite_threads ; serverThreads=2*serverThreads))
+{
+	echo $serverThreads" Threads"
 
-# 	for ((i=1 ; $i<=$repetitions ; i++))		
-# 	{
-# 		echo $i" repetition"
-# 		newFile="perfStat_devNullMultiThread_"$serverThreads"_"$i".data"
-# 		perf stat -e r500104,r500204,r500404,r500105,r500205,r500405,r500420,r500820,r530451,r530251,r530851,r500108,r500208,r500109,r500209,r50010a,r50020a,r50040a,r50080a,r500f0a,r50010b,r50020b,r50040b,r50080b,r50100b,r501f0b,r530426,r530126,r530326,r530526 -o $newFile -- ./dev_null $maxPackages $serverThreads >> /dev/null &
-# 		pid=$!
-# 		wait $pid
-# 	}
+	for ((i=1 ; $i<=$repetitions ; i++))		
+	{
+		echo $i" repetition"
+		newFile="perfStat_devNullMultiThread_"$serverThreads"_"$i".data"
+		perf stat -e r500104,r500204,r500404,r500105,r500205,r500405,r500420,r500820,r530451,r530251,r530851,r500108,r500208,r500109,r500209,r50010a,r50020a,r50040a,r50080a,r500f0a,r50010b,r50020b,r50040b,r50080b,r50100b,r501f0b,r530426,r530126,r530326,r530526 -o $newFile -- ./dev_null $maxPackages $serverThreads >> /dev/null &
+		pid=$!
+		wait $pid
+	}
 
-# }
+}
 
-# python postProcessing.py
-# mv SummaryResults.csv SummaryResults_devnull.csv
-# mv FullResults.csv FullResults_devnull.csv
-# rm *.data
+python postProcessing.py
+mv SummaryResults.csv SummaryResults_devnull.csv
+mv FullResults.csv FullResults_devnull.csv
+rm *.data
 
 echo ""
 echo "Testing UDP SingleThread Transmission"
