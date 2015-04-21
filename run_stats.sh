adca5006ffefdf50759fb5fc06ba7d37c895410b
Sebastian <sebablasko@gmail.com>
2015-04-21 15:46:16 -0300

    fixed filenames

--- run_stats.sh
+++ run_stats.sh
@@ -60,7 +60,7 @@ for ((serverThreads=1 ; $serverThreads<=$limite_threads ; serverThreads=2*server
 		echo $i" repetition"
 		#newFile="perfStat_devNullMultiThread_"$serverThreads"_"$i".data"
 
-		newFile="perfStat_devNullMultiThread_"
+		newFile="perfStat_devNullMultiThread"
 		
 		if (($serverThreads >= 0 & $serverThreads < 10)); then newFile=$newFile"_00"$serverThreads; fi
 		if (($serverThreads >= 10 & $serverThreads < 100)); then newFile=$newFile"_0"$serverThreads; fi
@@ -111,7 +111,7 @@ for ((serverSockets=1 ; $serverSockets<=$limite_threads ; serverSockets=2*server
 			if (($k >= 0 & $k < 10)); then newFile=$newFile"_00"$k".data"; fi
 			if (($k >= 10 & $k < 100)); then newFile=$newFile"_0"$k".data"; fi
 			if (($k >= 100)); then newFile=$newFile"_"$k".data"; fi
-				
+
 			#perf stat -e r500104,r500204,r500404,r500105,r500205,r500405,r500420,r500820,r530451,r530251,r530851,r500108,r500208,r500109,r500209,r50010a,r50020a,r50040a,r50080a,r500f0a,r50010b,r50020b,r50040b,r50080b,r50100b,r501f0b,r530426,r530126,r530326,r530526 -o $newFile -- ./server $packages 1 $(($k+1820)) &
 			perf stat -e r500104,r500204,r500404,r500105,r500205,r500405,r500420,r500820,r530451,r530251,r530851,r500108,r500208,r500109,r500209,r50010a,r50020a,r50040a,r50080a,r500f0a,r50010b,r50020b,r50040b,r50080b,r50100b,r501f0b,r530426,r530126,r530326,r530526,r5304b4,r5301b4,r5302b4,r5304b3,r5301b3,r5302b3,r5301b8,r5302b8,r5304b8,r530451,r530251,r530851,r530151,r500143,r500243,r500141,r500241,r500441,r500841,r501041,r502041,r500741,r503841,r500140,r500240,r500440,r500840,r501040,r502040,r500740,r503840,r500106,r500206,r500406,r500806,r501006,r502006,r500107,r500207,r500407,r500807,r501007,r502007,r502407,r500132,r500232,r500432,r500732 -o $newFile -- ./server $packages 1 $(($k+1820)) &
 			echo "Server listening at port "$(($k+1820))
