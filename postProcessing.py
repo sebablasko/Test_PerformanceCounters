09687d1754db548dce526eacd3b00c419477036e
Sebastian <sebablasko@gmail.com>
2015-04-21 14:36:48 -0300

    fixing

--- postProcessing.py
+++ postProcessing.py
@@ -7,53 +7,54 @@ def promedio(l, div):
 	else:
 		return float(str(l[0]).replace(",",""))
 
-files = glob.glob(os.getcwd()+"/*.data")
-eventos = {}
+def getDataFileName(archivo):
+	return os.path.basename(archivo.name).split("_")[4].split(".")[1]
 
+def getThreadsInDataFileName(archivo):
+	return int(os.path.basename(archivo.name).split("_")[2])
+
+def getRepetitionInDataFileName(archivo):
+	return int(os.path.basename(archivo.name).split("_")[3].split(".")[0])
+
+diccionarioEventos = {}
+
+files = glob.glob(os.getcwd()+"/*.data")
 for filename in sorted(files):
 	if(len(sys.argv)<2):
 		print "Error, debe ingresar como parametro el numero de repeticiones que incluyo la prueba"
 		exit()
+
 	repetitions = sys.argv[1]
 	archivo = open(filename, 'r')
-	threads = int(os.path.basename(archivo.name).split("_")[2])
-	repetition = os.path.basename(archivo.name).split("_")[3].split(".")[0]
+	threads = getThreadsInDataFileName(archivo)
+	repetition = getRepetitionInDataFileName(archivo)
 
 	for line in archivo:
-		if "%" in line: 									# Es un registro de evento
-			registro = line.split(" ")
-			registro = filter(lambda x: x!="", registro)
-			registro = registro[0:2]						#registro=[TotalApariciones, CodigoEvento]
+		if "%" in line: 											# Es un registro de evento
+			registro = filter(lambda x: x!="", line.split(" "))		# Elimino elementos vacios de la lista
+			registro = registro[0:2]								# registro=[TotalApariciones, CodigoEvento]
+			registro[0] = int(registro[0].replace(".",""))
 
-			if registro[1] not in eventos:
-				eventos[registro[1]] = {}
-			if threads not in eventos[registro[1]]:
-				eventos[registro[1]][threads] = []
-			eventos[registro[1]][threads].append(registro[0].replace(".",""))
+			if registro[1] not in diccionarioEventos:
+				diccionarioEventos[registro[1]] = {}
+			if threads not in diccionarioEventos[registro[1]]:
+				diccionarioEventos[registro[1]][threads] = []
+			diccionarioEventos[registro[1]][threads].append(registro[0])
 	archivo.close()
 
 #Concentrar registros en caso de udpmultisocket
 if len(os.path.basename(archivo.name).split("_")[4].split("."))>1:
-	for contador in eventos:
-		for thread in sorted(eventos[contador]):
-			nuevaLista = []
-			totalParcial = 0
-			for k in range(1, 1+len(eventos[contador][thread])):
-				totalParcial = totalParcial + int(eventos[contador][thread][k-1].replace(",",""))
-				if k%int(repetitions)==0:
-					nuevaLista.append(totalParcial)
-					totalParcial = 0
-			eventos[contador][thread] = nuevaLista
+	print "es multisockets"
 
 import pprint
-pprint.pprint(eventos, width=1)
+pprint.pprint(diccionarioEventos, width=1)
 
 #salida = open("FullResults.csv", "w+")
-for contador in eventos:
+for contador in diccionarioEventos:
 	salida.write(contador+"\n")
-	for thread in sorted(eventos[contador]):
+	for thread in sorted(diccionarioEventos[contador]):
 		salida.write(str(thread)+";")
-		for val in eventos[contador][thread]:
+		for val in diccionarioEventos[contador][thread]:
 			salida.write(val+";")
 		salida.write("\n")
 	salida.write("\n\n")
@@ -61,11 +62,11 @@ salida.close()
 
 
 summary = []
-for contador in eventos:
+for contador in diccionarioEventos:
 	actual = [contador]
-	for thread in sorted(eventos[contador]):
-		actual.append(promedio(eventos[contador][thread], repetitions))
-		actual.append(stddev(eventos[contador][thread], repetitions))
+	for thread in sorted(diccionarioEventos[contador]):
+		actual.append(promedio(diccionarioEventos[contador][thread], repetitions))
+		actual.append(stddev(diccionarioEventos[contador][thread], repetitions))
 	summary.append(actual)
 
 salida = open("SummaryResults.csv", "w+")
