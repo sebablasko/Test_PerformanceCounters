25eb3f13b7c3e899c55abafc2520f92e80b11985
sebastian <sebastian@sebastian-VirtualBox.(none)>
2015-04-21 22:23:57 -0300

    added mix results

---
+++ mixResults.py
@@ -0,0 +1,23 @@
+import json
+from pprint import pprint
+
+def getSummaryResultsFromRecord(eventCode, filename):
+	SummaryResultsFile = open(filename,'r')
+	for line in SummaryResultsFile:
+		if line.split(',')[0] == eventCode:
+			SummaryResultsFile.close()
+			return line.replace("\n","")
+	SummaryResultsFile.close()
+	return ""
+
+allFiles = ["SummaryResults_devnull.csv","SummaryResults_UDPMultiThread.csv","SummaryResults_UDPMultiSocket.csv"]
+
+with open('events.json') as data_file:
+	events = json.load(data_file)
+
+for event in events:
+	print(event["Name"] + ": " + event["Description"] + " (" + str(len(event["Codes"])) + ")")
+	for code in event["Codes"]:
+		print("\t" + code + "\t" + event["Codes"][code])
+		for summaryFile in allFiles:
+			print("\t\t"+summaryFile+"\t"+getSummaryResultsFromRecord(code,summaryFile))
