5a6b1bc6412d803147defd907658f8d152cac954
sebastian <sebastian@sebastian-VirtualBox.(none)>
2015-04-21 22:56:59 -0300

    added mix results

--- mixResults.py
+++ mixResults.py
@@ -10,16 +10,36 @@ def getSummaryResultsFromRecord(eventCode, filename):
 	SummaryResultsFile.close()
 	return ""
 
-allFiles = ["SummaryResults_devnull.csv","SummaryResults_UDPMultiThread.csv","SummaryResults_UDPMultiSocket.csv"]
+def saveRegistrosToPlot(titulo, datos, filename):
+	fullFilename = "plots/" + filename + ".dat"
+	archivo = open(fullFilename,'w+')
+	for val in datos:
+		archivo.write(val)
+		archivo.write(" ")
+		archivo.write(datos[val].replace(","," "))
+	archivo.close()
 
+
+# Crear carpeta para guardar fuentes de datos para graficos
+directory = "plots"
+if os.path.exists(directory):
+	os.makedirs(directory)
+
+# cargar datos de eventos y contadores desde json
 with open('events.json') as data_file:
 	events = json.load(data_file)
 
+# iterar sobre archivos rescatando y recolectando datos
+allFiles = ["SummaryResults_devnull.csv","SummaryResults_UDPMultiThread.csv","SummaryResults_UDPMultiSocket.csv"]
 for event in events:
-	print(event["Name"] + ": " + event["Description"] + " (" + str(len(event["Codes"])) + ")")
+	print(tituloGrafico + " (" + str(len(event["Codes"])) + ")")
 	for code in event["Codes"]:
 		print("\t" + code + "\t" + event["Codes"][code])
+		tituloGrafico = code + ": " + event["Codes"][code]
+		registros = {}
 		for summaryFile in allFiles:
 			record = getSummaryResultsFromRecord(code,summaryFile)
 			recordResults = record[record.find(",")+1:]
 			print("\t\t"+summaryFile+"\t"+recordResults)
+			registros[summaryFile] = recordResults
+		saveRegistrosToPlot(tituloGrafico, registros, code)
