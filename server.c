3df6372ce20c8f61c0a497e9082989f34906f2d0
Sebastian <sebablasko@gmail.com>
2015-08-12 14:49:51 -0300

    actualizados cliente/Servidor

--- server.c
+++ server.c
@@ -1,35 +1,124 @@
+#define _GNU_SOURCE
+
 #include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "../ssocket/ssocket.h"
 
+#include <sched.h>
+#include <unistd.h>
+
+#include <getopt.h>
+#include <string.h>
+
 //Definiciones
-#define BUF_SIZE 10
-#define FIRST_PORT 1820
+#define BUF_SIZE 512
+#define DEFAULT_PORT 1820
+// Nombres Schedulling
+#define equitativeSched "equitativeSched"
+#define dummySched "dummySched"
+#define pairSched "pairSched"
+#define impairSched "impairSched"
+#define numaPairSched "numaPairSched"
 
 //Variables
 int first_pack = 0;
 struct timeval dateInicio, dateFin;
 pthread_mutex_t lock;
 int mostrarInfo = 0;
-int MAX_PACKS = 1;
-int NTHREADS = 1;
-int DESTINATION_PORT = FIRST_PORT;
+int distribuiteCPUs = 0;
+int MAX_PACKS = 0;
+int NTHREADS = 0;
+int DESTINATION_PORT = DEFAULT_PORT;
+int reuseport = 0;
 double segundos;
+char* schedu = "SO";
+int cpuAssign = 0;
 
-/*
-	Esta version del servidor recibe: 
-		-cantidad de paquetes a enviar
-		-Numero de Threads a lanzar para compartir el socket
-		-puerto de bind para el socket
-*/
 
-//Metodo para Threads
-llamadaHilo(int socket_fd){
+void print_usage(){
+    printf("Uso: ./server [--verbose] --packets <num> --threads <num> --port <num> [--reuseport] [--scheduler <SchedSchemmaName> --setcpu <num>]\n");
+}
+
+void print_config(){
+    printf("Detalles de la prueba:\n");
+    printf("\tPuerto a escuchar:\t%d\n", DESTINATION_PORT);
+    printf("\tPaquetes a recibir:\t%d de %d bytes\n", MAX_PACKS, BUF_SIZE);
+    printf("\tUso de ReusePort:\t");
+    reuseport ? printf("Activado\n") : printf("Apagado\n");
+    printf("\tThreads que compartirán el socket:\t%d\n", NTHREADS);
+    printf("\tScheduller usado:\t%s\n",schedu);
+    printf("\tAsignación a CPU:\t");
+	strcmp(schedu,dummySched)==0 ? printf("CPU %d\n", cpuAssign) : printf("Segun SO\n");
+}
+
+void parseArgs(int argc, char **argv){
+	int c;
+	int digit_optind = 0;
+	while (1){
+		int this_option_optind = optind ? optind : 1;
+        int option_index = 0;
+
+		static struct option long_options[] = {
+			{"packets", required_argument, 0, 'd'},
+			{"threads", required_argument, 0, 't'},
+			{"port", required_argument, 0, 'p'},
+			{"scheduler", required_argument, 0, 's'},
+			{"setcpu", required_argument, 0, 'c'},
+			{"reuseport", no_argument, 0, 'r'},
+			{"verbose", no_argument, 0, 'v'},
+			{0, 0, 0, 0}
+		};
+
+         c = getopt_long (argc, argv, "vrd:t:p:s:c:",
+         long_options, &option_index);
+ 
+         if (c == -1)
+         	break; 
+
+         switch (c){
+			case 'v':
+				printf ("Modo Verboso\n");
+				mostrarInfo = 1;
+				break;
+
+			case 'r':
+				reuseport = 1;
+				break;
+
+			case 'd':
+				MAX_PACKS = atoi(optarg);
+				break;
+
+			case 't':
+				NTHREADS = atoi(optarg);
+				break;
+
+			case 'p':
+				DESTINATION_PORT = atoi(optarg);
+				break;
+
+			case 's':
+				schedu = optarg;
+				break;
+
+			case 'c':
+				cpuAssign = atoi(optarg);
+				break;
+
+			default:
+				printf("Error: La función getopt_long ha retornado un carácter desconocido. El carácter es = %c\n", c);
+				print_usage();
+				exit(1);
+         }
+	}
+}
+
+void llamadaHilo(int socket_fd){
 	char buf[BUF_SIZE];
 	int lectura;
 
-	if(mostrarInfo) printf("Socket Operativo: %d\n", socket_fd);
+	if(mostrarInfo) printf("Socket Operativo: %d, \t CPU: %d\n", socket_fd, sched_getcpu());
 
 	int i;
 	int paquetesParaAtender = MAX_PACKS/NTHREADS;
@@ -52,49 +141,92 @@ llamadaHilo(int socket_fd){
 			pthread_mutex_unlock(&lock);
 		}
 	}
+
+	if(mostrarInfo) printf("Fin Socket Operativo: %d, \t CPU: %d\n", socket_fd, sched_getcpu());
 }
 
 int main(int argc, char **argv){
-	//Verificar Parametros Entrada
-	if(argc <4){
-		fprintf(stderr,"Syntax Error: Esperado: ./server MAX_PACKS NTHREADS DESTINATION_PORT\n");
+
+	// Paso 1.- Parsear Argumentos
+	parseArgs(argc, argv);
+
+	// Paso 2.- Validar Argumentos
+	if(MAX_PACKS < 1 || NTHREADS < 1){
+		printf("Error en el ingreso de parametros\n");
+		print_usage();
 		exit(1);
 	}
 
-	//Recuperar total de paquetes a enviar
-	MAX_PACKS = atoi(argv[1]);
+	if(mostrarInfo)	print_config();
+	if(mostrarInfo)	printf("El pid es %d\n", getpid());
 
-	//Recuperar numero de Threads
-	NTHREADS = atoi(argv[2]);
-	pthread_t pids[NTHREADS];
+	// Paso 2.1.- Recuperar Total CPUs
+	int totalCPUs = sysconf(_SC_NPROCESSORS_ONLN);
+	if(mostrarInfo) printf("Total de Procesadores disponibles: %d\n", totalCPUs);	
 
-	//Recuperar puerto destino
-	DESTINATION_PORT = atoi(argv[3]);
-
-	//Crear Socket
+	// Paso 3.- Crear el Socket
 	int socket_fd;
 	char ports[10];
 	sprintf(ports, "%d", DESTINATION_PORT);
-	socket_fd = udp_bind(ports);
+	socket_fd = reuseport ? udp_bind_reuseport(ports) : udp_bind(ports);
 	if(socket_fd < 0) {
 		fprintf(stderr, "Error de bind al tomar el puerto\n");
 		exit(1);
 	}
-
 	pthread_mutex_init(&lock, NULL);
-	if(mostrarInfo)	printf("Usando %d threads, %d packs\n", NTHREADS, MAX_PACKS);
 
-	//Lanzar Threads
+	// Paso 4.- Configurar Threads
+	pthread_t pids[NTHREADS];
+    pthread_attr_t attr;
+    cpu_set_t cpus;
+    pthread_attr_init(&attr);	
+
+	// Paso 4.1.- Lanzar Threads segun el mecanismo de afinidad definido
 	int i;
 	for(i=0; i < NTHREADS; i++) {
-		pthread_create(&pids[i], NULL, llamadaHilo, socket_fd);
+		if(strcmp(schedu,equitativeSched)==0){
+				/* Caso 1: Afinidad Equitativa de threads entre las cpu */
+				CPU_ZERO(&cpus);
+				CPU_SET(i%totalCPUs, &cpus);
+				pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+				pthread_create(&pids[i], &attr, llamadaHilo, socket_fd);
+		}else if(strcmp(schedu,dummySched)==0){
+				/* Caso 2: Afinidad Dummy, asigna todos los threads al cpu definido en cpuAssign */
+				CPU_ZERO(&cpus);
+				CPU_SET(cpuAssign, &cpus);
+				pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+				pthread_create(&pids[i], &attr, llamadaHilo, socket_fd);
+		}else if(strcmp(schedu,pairSched)==0){
+				/* Caso 3: Pair Affinity, asigna todos los threads a cpus con numeros pares */
+				CPU_ZERO(&cpus);
+				CPU_SET((2*i)%totalCPUs, &cpus);
+				pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+				pthread_create(&pids[i], &attr, llamadaHilo, socket_fd);
+		}else if(strcmp(schedu,impairSched)==0){
+				/* Caso 4: Impair Affinity, asigna todos los threads a cpus con numeros impares */
+				CPU_ZERO(&cpus);
+				CPU_SET((2*i+1)%totalCPUs, &cpus);
+				pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+				pthread_create(&pids[i], &attr, llamadaHilo, socket_fd);
+		}else if(strcmp(schedu,numaPairSched)==0){
+				/* Caso 5: Numa Pair Affinity, asigna todos los threads a cpus con numeros pares considerando numeracion NUMA */
+				int j;
+				j = (i%2)==0 ? i : (i-1) + totalCPUs/2;
+				CPU_ZERO(&cpus);
+				CPU_SET((i%2)*totalCPUs/2, &cpus);
+				pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+				pthread_create(&pids[i], &attr, llamadaHilo, socket_fd);
+		}else{
+				/* Caso 6: NoAffinity, el sistema asigna los movimientos de threads en CPU*/
+				pthread_create(&pids[i], NULL, llamadaHilo, socket_fd);
+		}
 	}
 
-	//Esperar Threads
-	for(i=0; i < NTHREADS; i++) 
+	// Paso 4.2.- Esperar Threads
+	for(i=0; i < NTHREADS; i++)
 		pthread_join(pids[i], NULL);
 
-	//Medir Fin
+	// Paso 5.- Medir Fin
 	gettimeofday(&dateFin, NULL);
 
 	//Cerrar Socket
@@ -105,7 +237,7 @@ int main(int argc, char **argv){
 		printf("Tiempo Total = %g\n", segundos);
 		printf("QPS = %g\n", MAX_PACKS*1.0/segundos);
 	}else{
-		printf("%g, \n", segundos);
+		printf("%g, ", segundos);
 	}
 	exit(0);
 }
